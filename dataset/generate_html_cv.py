a13c47245be4d13f4190d21fa74f89e28127b8f0
Alexander Sivura <asivura@icloud.com>
2024-08-30 23:21:49 -0700

    Generate html for cvs

--- dataset/generate_html_cv.py
+++ dataset/generate_html_cv.py
@@ -6,12 +6,14 @@ from dotenv import load_dotenv
 from langchain_core.pydantic_v1 import BaseModel, Field
 from tqdm import tqdm
 import shutil
+from pydantic import BaseModel
+from typing import List
 
 # Load environment variables from .env file
 load_dotenv()
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-TEMPLATE_FILEPATH = os.path.join(SCRIPT_DIR, "convert_text_cv_to_markdown.prompt")
+TEMPLATE_FILEPATH = os.path.join(SCRIPT_DIR, "generate_html_cv.prompt")
 
 with open(TEMPLATE_FILEPATH, "r") as file:
     PROMPT_TEMPLATE = file.read()
@@ -25,25 +27,24 @@ def recreate_directory(directory_path):
     print(f"Created new directory: {directory_path}")
 
 
-class Resume(BaseModel):
-    """Resume in Markdown format."""
-
-    md: str = Field(description="Resume in markdown format.")
-
 prompt = PromptTemplate.from_template(PROMPT_TEMPLATE)
-llm = ChatOpenAI(model="gpt-4o", temperature=0.)
-structured_llm = llm.with_structured_output(Resume)
+llm = ChatOpenAI(model="gpt-4o", temperature=1.)
 
-llm_chain = prompt | structured_llm
+llm_chain = prompt | llm
 
 data_dir = os.getenv("DATA_DIR")
 
+input_dir = os.path.join(data_dir, "generated_cv_md")
+
 ds = pd.read_csv(os.path.join(data_dir, "sample.csv"))
-output_dir = os.path.join(data_dir, "formatted_cv")
+output_dir = os.path.join(data_dir, "generated_cv_html")
 recreate_directory(output_dir)
 
 for i, row in tqdm(ds.iterrows(), total=len(ds)):
-    cv_text = row['Text']
+
+    with open(os.path.join(input_dir, str(row.id)+".md"), "r") as input_file:
+        cv_text = input_file.read()
+
     cv = llm_chain.invoke({"CV_TEXT": cv_text})
-    with open(os.path.join(output_dir, str(row.id)+".md"), "w") as output_file:
-        output_file.write(cv.md)
+    with open(os.path.join(output_dir, str(row.id)+".html"), "w") as output_file:
+        output_file.write(cv.content)
